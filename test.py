5d99b933ea176aa0147a12b8980ce4b9bec636b5
B-Behzod <b.behzod1213@gmail.com>
2022-06-18 18:39:41 +0300

    My second assignment

--- test.py
+++ test.py
@@ -1,44 +1,29 @@
-print(4 * (6 + 5))
-print(4 * 6 + 5 )
-print(4 + 6 * 5 )
+st = 'Print only the words that start with s in this sentence'
+for word in st.split():
+    if word[0] == 's':
+        print(word)
 
-a = 3 + 1.5 + 4
-print(type(a))
+for num in range(0,11,2):
+    print(num)
 
-b = 'hello'
-print(b[1])
-print(b[::-1])
-print(b[4])
-print(b[-1])
+myls = [x for x in range(1,51) if x%3 ==0]
+print(myls)
 
-list = [0, 0, 0]
-print(list)
+st = 'Print every word in this sentence that has an even number of letters'
+for word in st.split():
+    if len (word)%2 == 0:
+        print(word+' even!')
 
-d = {'simple_key':'hello'}
-print(d['simple_key'])
-d = {'k1':{'k2':'hello'}}
-print(d['k1']['k2'])
-d = {'k1':[{'nest key':['this is deep',['hello']]}]}
-print(d['k1'][0]['nest key'][1][0])
-#d = {'k1':[1,2,{'k2'['this is tricky',{'tough':[1,2,['hello']]}]}]}
-#print(d['k1'])
+for num in range(1,101):
+    if num %3 == 0 and num %5 == 0:
+        print("FizzBuzz")
+    elif num %3 ==0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
 
-t = (5,8,9)
-print(t)
-
-list5 =[1,2,2,33,4,4,11,22,3,3,2]
-print(set(list5))
-
-e = 2 > 3
-print(e)
-e = 3 <= 2
-print(e)
-e = 3 == 2.0
-print(e) 
-e = 3.0 == 3
-print(e)
-e = 4**0.5 != 2
-print(e)
-l_one = [1,2,[3,4]]
-l_two = [1,2,{'k1':4}]
-print(l_one[2][0] >= l_two[2]['k1'])
+st = 'Create a list of the first letters of every word in this string'
+for word in st.split():
+    print(word[0])
